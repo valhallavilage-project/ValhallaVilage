7aad47b3c2a3299fbbfbda6a2d8c9314e41a7699
VladimirMirMir <miroschnikovvladimir@yandex.ru>
2025-08-26 00:17:15 +0300

    Fix environment lighting

--- Assets/PROJECTS/L2Farm/Features/DayNight/EnvironmentLighting.cs
+++ Assets/PROJECTS/L2Farm/Features/DayNight/EnvironmentLighting.cs
@@ -6,10 +6,14 @@ namespace L2Farm.Features.DayNight
     {
         [SerializeField] private float baseIntensity = 10;
         [SerializeField] private Light pointLight;
+        [SerializeField] private GameObject vfx;
 
         protected override void Evaluate(float evaluation)
         {
-            pointLight.intensity = baseIntensity * Mathf.RoundToInt(evaluation);
+            int mode = 1 - Mathf.RoundToInt(evaluation);
+            Debug.Log($"[{nameof(EnvironmentLighting)}] Result : {mode} from Evaluation {evaluation};");
+            pointLight.intensity = baseIntensity * mode;
+            vfx?.SetActive(mode > 0);
         }
     }
 }
